c277da0c24880dce78040f0e9daf5d9e15ef66f7
纪成10192096 <ji.cheng@zte.com.cn>
2017-04-20 11:32:52 +0800

    修改了先链对中的私有类的方法

--- src/main/java/algorithm/ListQueue.java
+++ src/main/java/algorithm/ListQueue.java
@@ -13,21 +13,6 @@ public class ListQueue<T> {
             this.next = next;
         }
 
-        public T getData() {
-            return data;
-        }
-
-        public void setData(T data) {
-            this.data = data;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
     }
 
     private Node begin;
