24361a4d087a5a02ba9967e88b1fa9aa7cd50ff9
jicheng-home <jicheng3385600@163.com>
2017-04-24 22:58:00 +0800

    使用深度优先遍历求子集

---
+++ src/main/java/algorithm/SubSets2.java
@@ -0,0 +1,37 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by jicheng on 2017/4/24.
+ * 使用算法课上老师使用的深度优先遍历的方式求解
+ */
+public class SubSets2 {
+    public ArrayList<ArrayList<Integer>> solution(int[] nums) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        if (nums == null) {
+            return result;
+        }
+        if (nums.length == 0) {
+            result.add(new ArrayList<Integer>());
+            return result;
+        }
+        Arrays.sort(nums);
+        ArrayList<Integer> subset = new ArrayList<>();
+        solution(nums, 0, subset, result);
+        return result;
+    }
+
+    private void solution(int[] nums,
+                          int begin,
+                          ArrayList<Integer> subset,
+                          ArrayList<ArrayList<Integer>> result) {
+        result.add(new ArrayList<>(subset));
+        for (int i = begin; i < nums.length; i++) {
+            subset.add(nums[i]);
+            solution(nums, i + 1, subset, result);
+            subset.remove(subset.size() - 1);
+        }
+    }
+}
