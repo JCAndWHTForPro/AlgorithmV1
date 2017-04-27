3cf7f76f953a3bca473d8fa46e4aad1db88e46a9
jicheng-home <jicheng3385600@163.com>
2017-04-27 15:06:47 +0800

    提交相关

---
+++ src/main/java/algorithm/Permute.java
@@ -0,0 +1,49 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by jicheng on 2017/4/27.
+ */
+public class Permute {
+    public List<List<Integer>> solution(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) {
+            return result;
+        }
+        if (nums.length == 0) {
+            result.add(new ArrayList<Integer>());
+            return result;
+        }
+        boolean[] flags = new boolean[nums.length];
+        Arrays.fill(flags, false);
+        List<Integer> subset = new ArrayList<>();
+        solution(subset, flags, nums, result);
+        return result;
+    }
+
+    private void solution(List<Integer> subset, boolean[] flags, int[] nums, List<List<Integer>> result) {
+        if (subset.size() == nums.length) {
+            result.add(new ArrayList<Integer>(subset));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (flags[i]) {
+                continue;
+            }
+            subset.add(nums[i]);
+            flags[i] = true;
+            solution(subset, flags, nums, result);
+            subset.remove(subset.size() - 1);
+            flags[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> result = new Permute().solution(new int[]{1,2,3});
+        for(List<Integer> it : result){
+            System.out.println(it.toString());
+        }
+    }
+}
